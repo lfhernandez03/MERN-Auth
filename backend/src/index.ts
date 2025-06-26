d044d474345b187ad520e9ff85cb6e30609b8e5b
lfhernandez03 <Luishsg1@gmail.com>
2025-06-26 17:41:51 -0500

    Initial Config

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hola Mundo")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at PORT ${PORT}`)
+})
